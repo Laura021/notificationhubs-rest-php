bf707e0ac2dcf19aed88572395069850e2463e21
Laura Trejo <laura.trea@gmail.com>
2020-04-02 18:35:34 -0600

    Add Laravel Service Provider Class

---
+++ src/NotificationHubsRest/Laravel/ServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace NotificationHubsRest\Laravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class ServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //NPI
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //NPI
+    }
+}
